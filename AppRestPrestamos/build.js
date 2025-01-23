5570e4e7f9612113fc429ddf831f7e9d4eee16b9
AxelsBernal <AxsandovalBe@ittepic.edu.mx>
2025-01-23 15:22:49 -0700

    Usar npx en build.js para evitar problemas de permisos

--- AppRestPrestamos/build.js
+++ AppRestPrestamos/build.js
@@ -1,13 +1,8 @@
 const { execSync } = require('child_process');
-const path = require('path');
 
 try {
-    // Construye la ruta al ejecutable de Babel de manera multiplataforma
-    const babelPath = path.join('node_modules', '.bin', 'babel');
-    const command = `${babelPath} src --out-dir dist`;
-
-    // Ejecuta el comando
-    execSync(command, { stdio: 'inherit', shell: true });
+    // Ejecuta Babel con npx para evitar problemas de permisos
+    execSync('npx babel src --out-dir dist', { stdio: 'inherit', shell: true });
     console.log('Build completed successfully.');
 } catch (error) {
     console.error('Build failed:', error);
